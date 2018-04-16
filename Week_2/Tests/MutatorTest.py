97c72206cbd61f67092af51597cca3c9b9ce1903
Matthlas <mr.mcrichter@gmail.com>
2018-04-17 00:37:39 +0200

    Implemented _calculate_fitness() --> I used the normalized fitness I implemented for my problem. We can talk about whether that's a good idea.
    I also added to the ZeroInitializer a little tweak changing one of the 0's to an other random valid value since the
    fitness of all individuals would be 0 then (for my fitness function at least) which leads to problem with roulette
    selection.
    TODO: Change testZeroInitializer
    
    Added a Test for the Mutator

---
+++ Week_2/Tests/MutatorTest.py
@@ -0,0 +1,26 @@
+import unittest
+import Week_2.Mutator as Mutator
+import random
+
+
+class InitializerTest(unittest.TestCase):
+    def setUp(self):
+        self.mutation_probability = 0.5
+        self.machine_count = 5
+        self.chromosome = [3, 3, 0, 2, 4, 3, 3]
+
+    def testBitFlipMutator(self):
+        random.seed(0)
+        mutated_gene = Mutator.BitFlipMutator(self.mutation_probability, self.machine_count).mutate(self.chromosome)
+        self.assertEqual(len(self.chromosome), len(mutated_gene))
+        self.assertEqual([3, 3, 2, 2, 2, 3, 1], mutated_gene)
+
+    def testSwapMutator(self):
+        random.seed(0)
+        mutated_gene = Mutator.SwapMutator(self.mutation_probability, self.machine_count).mutate(self.chromosome)
+        self.assertEqual(len(self.chromosome), len(mutated_gene))
+        self.assertEqual([3, 3, 2, 4, 0, 3, 3], mutated_gene)
+
+
+if __name__ == '__main__':
+    unittest.main()
