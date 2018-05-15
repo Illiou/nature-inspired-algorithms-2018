b5976db2a14213fed4f63a7b2a2a30825d4572ee
Illiou <manny123@hotmail.de>
2018-05-15 03:25:10 +0200

    added a few comments

--- Week_3/Emanuel/TSPACO.py
+++ Week_3/Emanuel/TSPACO.py
@@ -18,6 +18,7 @@ class TSPACO:
         self.beta = beta
         self.ant_number = ant_number
         self.n_best_to_intensify = n_best_to_intensify
+        # minimal and maximal values for pheromones which didn't turn out to be so great
         self.pher_min = pher_min
         self.pher_max = pher_max
 
@@ -32,6 +33,7 @@ class TSPACO:
             # print(f"Iteration {i}")
             paths = self.construct_solutions()
             self.evaporate()
+            # calculate indices for best paths, then take N and intensify them
             path_qualities = np.argsort([self.objective_function(path) for path in paths])
             best_paths = paths[path_qualities[:self.n_best_to_intensify]]
             self.intensify(best_paths)
@@ -45,18 +47,24 @@ class TSPACO:
         self.pheromone_matrix = np.full_like(self.distance_matrix, self.initialization_value, dtype=float)
 
     def construct_solutions(self):
+        # calculate value (not technically a probability) for each city to go to any other city, without normalization
         city_probabilities = self.pheromone_matrix.copy()
         if self.alpha == 1 and self.beta == 1:
             city_probabilities *= self.heuristic_matrix
         elif self.alpha != 1 or self.beta != 0:
             city_probabilities = city_probabilities ** self.alpha * self.heuristic_matrix ** self.beta
+        # calculate path for each ant
         paths = np.zeros((self.ant_number, self.cities), dtype=int)
         for k in range(self.ant_number):
             cities_left = city_probabilities.copy()
+            # random starting city
             i = random.randrange(0, self.cities)
             paths[k, 0] = i
             for cnt in range(1, self.cities):
+                # set current city "probability" to 0 for all cities since it can't go back there
                 cities_left[:, i] = 0
+                # calculate cumulative sum for row of current city, then get random number of up to the total sum and
+                # find point/index of it within cumulative sum, which is then the selected city
                 cum_sum = np.cumsum(cities_left[i])
                 i = bisect.bisect_left(cum_sum, random.uniform(0, cum_sum[-1]))
                 paths[k, cnt] = i
