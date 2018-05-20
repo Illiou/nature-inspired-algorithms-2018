343199f6b6cf1cdde1ab493d58826d6233e2d501
Illiou <manny123@hotmail.de>
2018-05-20 22:59:14 +0200

    added best solution visualization

--- Week_3/Emanuel/force_graph_json_gen.py
+++ Week_3/Emanuel/force_graph_json_gen.py
@@ -1,7 +1,7 @@
 import numpy as np
 import json
 
-problem = 3
+problem = 2
 distances_file = f"../TSP_Problems/problem_0{problem}.tsp"
 distance_matrix = np.loadtxt(distances_file)
 
@@ -19,3 +19,16 @@ data = {"nodes": nodes, "links": links}
 
 with open("force_data.json", "w") as file:
     json.dump(data, file, indent=1)
+
+
+# best solutions
+def objective_function(solution):
+    return sum(distance_matrix[solution[i], solution[(i + 1) % len(solution)]] for i in range(len(solution)))
+
+solutions_file = "best_solutions.csv"
+solutions = np.loadtxt(solutions_file, dtype=int, delimiter=",")
+
+path_qualities = [objective_function(path) for path in solutions]
+best_path = np.argmin(path_qualities)
+print(path_qualities[best_path])
+print(solutions[best_path])
