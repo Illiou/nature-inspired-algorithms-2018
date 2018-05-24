e0f766dfa8b5eedc54e5c421acda821b0e7258eb
Illiou <manny123@hotmail.de>
2018-05-24 15:39:07 +0200

    fixed some bugs, added preliminary testing

--- Week_4/Emanuel/DifferentialEvolution.py
+++ Week_4/Emanuel/DifferentialEvolution.py
@@ -24,11 +24,13 @@ class DifferentialEvolution(ABC):
         pass
 
     def run(self, generations=1):
-        self.initialize()
+        best_objective_fs = np.zeros(generations)
         for g in range(generations):
             donors = self.mutate()
             trials = self.crossover(donors)
             self.select(trials)
+            best_objective_fs[g] = np.apply_along_axis(self.objective_function, 1, self.population).min()
+        return best_objective_fs
 
     def initialize(self):
         rand_arr = np.random.rand(self.population_size, self.chromosome_size)
@@ -50,10 +52,12 @@ class DifferentialEvolution(ABC):
         for i in range(self.population_size):
             x1, x2, x3 = unique_rands(self.population_size, 3, [i])
             donors[i] = self.population[x1] + self.scale_factor * (self.population[x2] - self.population[x3])
-        return donors
+        return np.maximum(self.lower_bounds, donors)
 
     def crossover(self, donors):
         to_crossover = np.random.rand(self.population_size, self.chromosome_size) <= self.crossover_rate
+        for i, j in enumerate(np.random.randint(self.chromosome_size, size=self.population_size)):
+            to_crossover[i, j] = True
         return np.where(to_crossover, donors, self.population)
 
     def select(self, trials):
--- Week_4/Emanuel/PowerPlantDE_test.py
+++ Week_4/Emanuel/PowerPlantDE_test.py
@@ -1,5 +1,6 @@
-import numpy as np
 from Week_4.Emanuel.PowerPlantDE import PowerPlantDE
+import numpy as np
+import matplotlib.pyplot as plt
 
 problem_1 = {"c": [10000, 80000, 400000],
              "p": [0.45, 0.25, 0.2],
@@ -27,15 +28,28 @@ problem_3 = {"c": [10000, 80000, 400000],
 
 # Parameters
 problem = problem_1
-population_size = 20
+population_size = 50
 scale_factor = 0.5
 crossover_rate = 0.6
 lower_bounds = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-upper_bounds = [3, 3, 3, 1000, 1000, 1000, 10, 10, 10]
+energy_up = min(problem["k"][i] * problem["m"][i] for i in range(len(problem["k"])))
+sold_up = max(problem["d"])
+price_up = max(problem["p"])
+upper_bounds = [energy_up, energy_up, energy_up, sold_up, sold_up, sold_up, price_up, price_up, price_up]
 plants = list(zip(problem["k"], problem["c"], problem["m"]))
 markets = list(zip(problem["p"], problem["d"]))
 purchase_price = problem["cost price"]
 
-pp_de = PowerPlantDE(population_size, scale_factor, crossover_rate, lower_bounds, upper_bounds, plants, markets, purchase_price)
+pp_de = PowerPlantDE(population_size, scale_factor, crossover_rate, lower_bounds, upper_bounds,
+                     plants, markets, purchase_price)
 
-pp_de.run(10)
+generations = 500
+
+best_profits = - pp_de.run(generations)
+
+curr_fig, curr_ax = plt.subplots()
+curr_ax.plot(best_profits, label="Best objective function")
+curr_ax.axhline(problem["benchmark"], linestyle="dashed", label="Benchmark")
+curr_ax.set(title="Problem 1", xlabel="Generations", ylabel="Best objective function")
+curr_ax.legend()
+curr_fig.show()
