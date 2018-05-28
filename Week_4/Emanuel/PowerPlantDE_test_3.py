14780e809b1928c6a2baa287765f4e8189ebaa82
Jonathan Loos <jonathan.loos@web.de>
2018-05-28 22:52:28 +0200

    Add test3

---
+++ Week_4/Emanuel/PowerPlantDE_test_3.py
@@ -0,0 +1,65 @@
+from Week_4.Emanuel.PowerPlantDE import PowerPlantDE
+import matplotlib.pyplot as plt
+import numpy as np
+
+problem_1 = {"c": [10000, 80000, 400000],
+             "p": [0.45, 0.25, 0.2],
+             "d": [2000000, 30000000, 20000000],
+             "k": [50000, 600000, 4000000],
+             "m": [100, 50, 3],
+             "cost price": 0.6,
+             "benchmark": 1514312}
+
+problem_2 = {"c": [10000, 80000, 400000],
+             "p": [0.45, 0.25, 0.2],
+             "d": [2000000, 30000000, 20000000],
+             "k": [50000, 600000, 4000000],
+             "m": [100, 50, 3],
+             "cost price": 0.1,
+             "benchmark": 1818406}
+
+problem_3 = {"c": [10000, 80000, 400000],
+             "p": [0.5, 0.3, 0.1],
+             "d": [1000000, 5000000, 5000000],
+             "k": [50000, 600000, 4000000],
+             "m": [100, 50, 3],
+             "cost price": 0.6,
+             "benchmark": 404041}
+
+if __name__ == '__main__':
+    # Parameters
+    problem = problem_1
+    population_size = 50
+    scale_factor = 0.5
+    crossover_rate = 0.7
+    energy_up = min(problem["k"][i] * problem["m"][i] for i in range(len(problem["k"])))
+    sold_up = max(problem["d"])
+    price_up = max(problem["p"])
+    upper_bounds = [energy_up, energy_up, energy_up, sold_up, sold_up, sold_up, price_up, price_up, price_up]
+    plants = list(zip(problem["k"], problem["c"], problem["m"]))
+    markets = list(zip(problem["p"], problem["d"]))
+    purchase_price = problem["cost price"]
+
+    generations = 3000
+    iterations = 100
+
+    all_best_profits = np.empty([iterations, generations])
+    benchmark_reached = 0
+    for iteration in range(iterations):
+        pp_de = PowerPlantDE(population_size, scale_factor, crossover_rate, upper_bounds,
+                             plants, markets, purchase_price)
+        profits = - pp_de.run(generations)
+        if profits[-1] >= problem["benchmark"]:
+            benchmark_reached += 1
+        all_best_profits[iteration] = profits
+    best_profits = np.mean(all_best_profits, axis=0)
+    print("best are {}".format(all_best_profits[:, -1]))
+    print("benchmark reached {} of {} times".format(benchmark_reached, iterations))
+
+    curr_fig, curr_ax = plt.subplots()
+    curr_ax.plot(best_profits, label="Best objective function")
+    curr_ax.axhline(problem["benchmark"], linestyle="dashed", label="Benchmark")
+    curr_ax.set(title="Problem 1", xlabel="Generations", ylabel="Best objective function")
+    curr_ax.legend()
+    # curr_fig.show()
+    plt.show()
