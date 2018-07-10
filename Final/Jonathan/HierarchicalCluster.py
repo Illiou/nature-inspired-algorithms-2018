4766a0ae9e5c80131ac018754d747ccd0df3e234
Jonathan Loos <jonathan.loos@web.de>
2018-07-10 20:21:38 +0200

    Finalize clustering

--- Final/Jonathan/HierarchicalCluster.py
+++ Final/Jonathan/HierarchicalCluster.py
@@ -1,7 +1,19 @@
 import numpy as np
-import scipy.spatial.distance as ssd
-import scipy.optimize as optimize
-import math
+
+
+# distance functions
+def d_mean(x, y):
+    return (x + y) / 2
+
+
+def d_min(x, y):
+    axis = np.argmax(x.shape)
+    return np.min(np.array([x, y]), axis=axis)
+
+
+def d_max(x, y):
+    axis = np.argmax(x.shape)
+    return np.max(np.array([x, y]), axis=axis)
 
 
 class Cluster:
@@ -49,7 +61,7 @@ class HierarchicalClustering:
         self.customer_count = distances.shape[0]
         self.distances = distances
         self.demands = demands
-        print(distances)
+        self.max_distance = np.max(self.distances)
 
     def _initial_clusters(self):
         clusters = []
@@ -57,47 +69,37 @@ class HierarchicalClustering:
             clusters.append(Cluster(index=i, demand=demand))
         return clusters
 
-    def cluster(self):
+    def cluster(self, dist_func=d_mean):
         clusters = self._initial_clusters()
         cluster_distances = np.array(self.distances)
-        # convert the redundant n*n square matrix form into a condensed nC2 array
-        # flattened_distances[{n choose 2}-{n-i choose 2} + (j-i-1)] is the distance between points i and j
-        flattened_distances = ssd.squareform(cluster_distances)
+
+        def set_new_distance(i, j, dist):
+            cluster_distances[i, j] = dist
+            cluster_distances[j, i] = dist
+
         while len(clusters) > 1:
-            nearest_cluster_index = np.argmin(flattened_distances)
-            cluster1_index, cluster2_index = self._flattened_index_to_two_d(nearest_cluster_index)
-            break
+            nearest_cluster_index = np.unravel_index(np.argmin(np.where(cluster_distances != 0,
+                                                                        cluster_distances, self.max_distance+1)),
+                                                     cluster_distances.shape)
+            print(nearest_cluster_index)
+            first_cluster_index = int(nearest_cluster_index[0])
+            first_cluster = clusters[first_cluster_index]
+            second_cluster_index = int(nearest_cluster_index[1])
+            second_cluster = clusters[second_cluster_index]
+            set_new_distance(*nearest_cluster_index, 0)
+            cluster_distances[first_cluster_index] = dist_func(cluster_distances[first_cluster_index],
+                                                               cluster_distances[second_cluster_index])
+            cluster_distances[:, first_cluster_index] = dist_func(cluster_distances[:, first_cluster_index],
+                                                                  cluster_distances[:, second_cluster_index])
 
-    def _calculate_start_index_of_row(self, row):
-        return row * self.customer_count - (row * (row + 1)) / 2
-
-    def _flattened_index_to_two_d(self, index):
-        very_small = 0.000000001  # to prevent strange rounding
-        row = math.floor(optimize.fsolve(lambda x: self._calculate_start_index_of_row(x) - index, np.array([0]))[0]
-                         + very_small)
-        column = math.floor(index - self._calculate_start_index_of_row(row) + row + 1 + very_small)
-        return row, column
-
-    def _two_d_to_flattened_index(self, row, column):
-        if row >= column:
-            raise AttributeError("column must be greater than row for upper triangle in matrix")
-        return self._calculate_start_index_of_row(row) + column - row
-
-
-def symmetric_distance_matrix(distances):
-    symmetric = np.copy(distances)
-    n = distances.shape[0]
-    if distances.shape[1] != n:
-        raise AttributeError("Not quadratic")
-    for x in range(n):
-        for y in range(n):
-            first = distances[x, y]
-            second = distances[y, x]
-            if first != second:
-                mean = np.mean([first, second])
-                symmetric[x, y] = mean
-                symmetric[y, x] = mean
-    return symmetric
+            mask_second_out = np.arange(cluster_distances.shape[0]) != second_cluster_index
+            cluster_distances = cluster_distances[mask_second_out]
+            cluster_distances = cluster_distances[:, mask_second_out]
+            clusters[first_cluster_index] = Cluster(cluster1=first_cluster, cluster2=second_cluster)
+            clusters = clusters[:second_cluster_index] + clusters[second_cluster_index+1:]
+            print(clusters)
+            print(cluster_distances)
+        return clusters
 
 
 if __name__ == '__main__':
@@ -107,11 +109,10 @@ if __name__ == '__main__':
     print(cluster1, cluster2, cluster3)
     path = "../Vehicle_Routing_Problems/VRP1/"
     distances = np.loadtxt(path + "distance.txt")
-    distances = symmetric_distance_matrix(distances)
     demands = np.loadtxt(path + "demand.txt", dtype=int)
-    test_city_count = 7
+    test_city_count = 9
     HC = HierarchicalClustering(distances[0:test_city_count, 0:test_city_count], demands[0:test_city_count])
     HC.cluster()
-    # from scipy.special import comb
-    # print([[comb(4, 2)-comb(4-i,2)+j-i for j in range(4)] for i in range(4)])
+    HC.cluster(dist_func=d_min)
+    HC.cluster(dist_func=d_max)
 
